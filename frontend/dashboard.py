93a95a0baf1e36d1e84ad57dbaef2468a693a4b8
Shashwat Raj <Shashwatraj8732@gmail.com>
2026-08-31 11:50:48 +0530

    Update dashboard.py

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 import os
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "backend"))
-
 import streamlit as st
 import pandas as pd
 import csv
